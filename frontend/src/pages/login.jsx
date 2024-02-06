6e17aef06005dffc649c92212f416bd9a96eef38
santyloaiza74 <santyloaiza74@gmail.com>
2024-02-06 15:17:37 -0500

    validando input email

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import validateUser from "../services/login/validate";
-import { Button, Card, Col, Container, Form, Row, Image } from "react-bootstrap"
-import {useNavigate} from 'react-router-dom'
+import { Button, Card, Col, Container, Form, Row, Image, Alert } from "react-bootstrap"
+import { useNavigate } from 'react-router-dom'
 import './login.css'
 import logo1 from './../img/logo1.png'
 
@@ -10,13 +10,13 @@ function Login() {
         email: '',
         password: ''
     })
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const handleSubmit = async () => {
         try {
-            const {token} = await validateUser(login)
-            const accesstoken= token
+            const { token } = await validateUser(login)
+            const accesstoken = token
             console.log(accesstoken)
-            localStorage.setItem('token',accesstoken)
+            localStorage.setItem('token', accesstoken)
             setLogin({
                 email: '',
                 password: ''
@@ -26,9 +26,9 @@ function Login() {
             console.log(error)
         }
     }
-    const handleChange=(event)=>{
-        const {name,value}=event.target
-        setLogin((validateUser)=>{
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setLogin((validateUser) => {
             return {
                 ...validateUser,
                 [name]: value
@@ -37,50 +37,48 @@ function Login() {
     }
     return (
         <>
-        <Container fluid>
+            <Container fluid>
 
-            <Row >
-                <Col col='12'>
+                <Row >
+                    <Col col='12'>
 
-                    <Card className='bg-light text-dark my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
-                        <Card.Body className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
-                            <img src={logo1}></img>
-                            <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                            <p className="text-dark-50 mb-5">Please enter your login and password!</p>
+                        <Card className='bg-light text-dark my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
+                            <Card.Body className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+                                <img src={logo1}></img>
+                                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                <p className="text-dark-50 mb-5">Please enter your login and password!</p>
 
-                            <Form>
+                                <Form>
 
-                                <Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <center>
+                                            <Form.Label>Email address</Form.Label>
+                                        </center>
+                                        <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <center>
+                                            <Form.Label>
+                                                Password
+                                            </Form.Label>
+                                        </center>
+                                        <Form.Control type="Password" name="password" onChange={handleChange} placeholder="Enter password" />
+                                    </Form.Group>
+
+                                    <br />
+
                                     <center>
-                                <Form.Label>
-                                        Email
-                                    </Form.Label>
-                                    </center>   
-                                    <Form.Control  type="email" name="email"  onChange={handleChange} />
-                                </Form.Group>
-                                <Form.Group>
-                                    <center>
-                                <Form.Label>
-                                        Password
-                                    </Form.Label>
+                                        <Button className="Buttonn btn" onClick={handleSubmit}>Log in</Button>
                                     </center>
-                                    <Form.Control  type="Password" name="password"  onChange={handleChange} />
-                                </Form.Group>
-                            
-                                <br/>
+                                </Form>
+                            </Card.Body>
+                        </Card>
 
-                                <center>
-                                <Button className="Buttonn btn" onClick={handleSubmit}>Log in</Button>
-                                </center>
-                            </Form>
-                        </Card.Body>
-                    </Card>
+                    </Col>
+                </Row>
 
-                </Col>
-            </Row>
-
-        </Container>
-    </>
+            </Container>
+        </>
     )
 }
 
