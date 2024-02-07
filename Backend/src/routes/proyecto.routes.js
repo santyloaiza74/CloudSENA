3e3bffa7dda73ae4d65febc68f801776d811bcfe
santyloaiza74 <santyloaiza74@gmail.com>
2024-02-07 16:39:35 -0500

    primer ensayos multer

--- Backend/src/routes/proyecto.routes.js
+++ Backend/src/routes/proyecto.routes.js
@@ -1,12 +1,26 @@
 const router = require('express').Router()
 const proyectoController = require('../controllers/proyecto.controller')
 const proyectoSchema = require('../database/models/proyecto.model')
+const multer=require('multer')
+const fs=require('node:fs')
 const controller = new proyectoController
+
+const uploads=multer({dest: 'public/upload'})
 
 router.get('/', async (req, res) => {
     const proyectos = await controller.index()
     res.json({ proyectos })
 })
+
+router.post('/multer',uploads.single('multer'),async(req,res)=>{
+    saveimage(req.file)
+    res.status(200).json({message: 'guardado'})
+})
+function saveimage(file){
+    const newpath=`public/upload/${file.originalname}`
+    fs.renameSync(file.path,newpath)
+    return newpath
+}
 
 router.post('/', async (req, res) => {
     const {nombre,autores,idficha,fecha} = req.body
