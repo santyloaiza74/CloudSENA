da0d80081ca8061377c43616db8a00e002a20725
Jean <jeankyt06@gmail.com>
2024-02-09 17:37:43 -0500

    Cards Proyectos

--- frontend/src/components/Principal/List.jsx
+++ frontend/src/components/Principal/List.jsx
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { Spinner, Table, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { Card, Button, Spinner } from 'react-bootstrap';
 import axios from 'axios';
+import './list.css'
+import logo1 from './../../img/logo1.png'
+
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+
 function List() {
-    const [proyecto, setPost] = useState([])
+    const [proyecto, setProyecto] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-        axios.get('http://127.0.0.1:3300/api/v1/proyecto').then((response) => {
-            setPost(response.data.proyectos);
-        });
+        axios.get('http://127.0.0.1:3300/api/v1/proyecto')
+            .then((response) => {
+                setProyecto(response.data.proyectos);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching projects:', error);
+                setLoading(false);
+            });
     }, []);
+
     return (
-        <Table striped bordered hover>
-            <thead>
-                <th>Nombre</th>
-                <th>Autores</th>
-                <th>Ficha</th>
-                <th>Fecha</th>
-                <th>Ruta</th>
-            </thead>
-            <tbody>
-                {proyecto.map(({ _id, nombre,autores, ficha, fecha, ruta}) => (
-                    <tr key={`user-row-${_id}`}>
-                        <td>{nombre}</td>
-                        <td>{autores}</td>
-                        <td>{ficha[0].nombre}</td>
-                        <td>{fecha}</td>
-                        <td>{ruta}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
-    )
+        <div className="card-container">
+            {loading ? (
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            ) : (
+                // Agrupa las cards en contenedores de filas
+                <div className="card-row">
+                    {proyecto.map(({ _id, nombre, autores, ficha, fecha, ruta }) => (
+                        <Card key={_id} className="custom-card-style">
+                            <Card.Body>
+                            <Card.Img variant="top" src={logo1} alt={`${nombre} Image`} />
+                                <Card.Title>{nombre}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{autores}</Card.Subtitle>
+                                <Card.Text>
+                                    <strong>Ficha:</strong> {ficha[0].nombre}<br />
+                                    <strong>Fecha:</strong> {fecha}<br />
+                                    <strong>Ruta:</strong> {ruta}
+                                </Card.Text>
+                                <Button className='Buttonn'>Ver Detalles</Button>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
-export default List
+
+export default List;
