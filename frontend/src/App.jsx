a1bdf662bb564e8393ee34c35e4dbcb395e1ab42
santyloaiza74 <santyloaiza74@gmail.com>
2024-05-30 23:22:42 -0500

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import Login from './pages/login'
 import Register from './pages/register'
 import  principal  from './pages/principal'
 import proyecto from './pages/Proyectos/CrearProyecto'
-import edieli from './pages/Proyectos/EditarProyecto'
 import Cficha from './pages/Fichas/CrearFicha'
 import fichas from './pages/Fichas/Fichas'
 import usuarios from './pages/Usuarios/Usuario'
@@ -23,7 +22,6 @@ function App() {
         <Route path='/login/register' Component={Register}></Route>
         <Route path='/login' Component={Login}></Route>
         <Route path='/proyecto' Component={proyecto}></Route>
-        <Route path='/edel' Component={edieli}></Route>
         <Route path='/ficha' Component={Cficha}></Route>
         <Route path='/fichas' Component={fichas}></Route>
         <Route path='/users' Component={usuarios}></Route>
